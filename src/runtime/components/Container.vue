25983dd23a05a5c4d9a98e10ec97930278ba1b37
Daniele Vinci <vinci.dani@gmail.com>
2025-09-09 20:56:16 -0400

    feat: add UI components and theme configuration for improved styling

---
+++ src/runtime/components/Container.vue
@@ -0,0 +1,5 @@
+<template>
+  <div :class="container({ class: props.class })">
+    <slot />
+  </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,58 @@ import ui from '@nuxt/ui/vite'
 export default defineConfig({
   plugins: [
     vue(),
-    ui(),
+    ui({
+      ui: {
+        colors: {
+          primary: 'green',
+          neutral: 'slate',
+          tertiary: 'indigo'
+        },
+        theme: {
+          colors: [
+            'primary',
+            'secondary',
+            'tertiary',
+            'info',
+            'success',
+            'warning',
+            'error'
+          ],
+          transitions: true,
+          defaultVariants: {
+            color: 'neutral',
+            size: 'sm'
+          }
+        },
+        // button: {
+        //   slots: {
+        //     base: 'font-bold'
+        //   }
+        // },
+        icons: {
+          arrowLeft: 'i-lucide-arrow-left',
+          arrowRight: 'i-lucide-arrow-right',
+          check: 'i-lucide-check',
+          chevronDoubleLeft: 'i-lucide-chevrons-left',
+          chevronDoubleRight: 'i-lucide-chevrons-right',
+          chevronDown: 'i-lucide-chevron-down',
+          chevronLeft: 'i-lucide-chevron-left',
+          chevronRight: 'i-lucide-chevron-right',
+          chevronUp: 'i-lucide-chevron-up',
+          close: 'i-lucide-x',
+          ellipsis: 'i-lucide-ellipsis',
+          external: 'i-lucide-arrow-up-right',
+          file: 'i-lucide-file',
+          folder: 'i-lucide-folder',
+          folderOpen: 'i-lucide-folder-open',
+          loading: 'i-lucide-loader-circle',
+          minus: 'i-lucide-minus',
+          plus: 'i-lucide-plus',
+          search: 'i-lucide-search',
+          upload: 'i-lucide-upload'
+        }
+      }
+    }),
     vueDevTools(),
   ],
   resolve: {
